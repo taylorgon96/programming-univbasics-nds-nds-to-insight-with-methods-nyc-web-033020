6a4002241363bbdd684f33d09374f00b4b99face
Taylor Gonzalez <taylorgon@gmail.com>
2020-03-12 19:24:50 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -36,7 +36,7 @@ end
 def directors_totals(nds)
  
    grand_total = {
-          { :name =>
+            {:name =>
             "#{nds[0][:name]}", 
             :movies => [nds[0][:movies][0][:worldwide_gross] + nds[0][:movies][1][:worldwide_gross] + nds[0][:movies][2][:worldwide_gross] + nds[0][:movies][3][:worldwide_gross] + nds[0][:movies][4][:worldwide_gross] + nds[0][:movies][5][:worldwide_gross]]} 
             {:name =>
